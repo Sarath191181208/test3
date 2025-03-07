24234ffddc86d278cf51d3c26bbce2bb5f1eef95
srinivasas-zluri <srinivasa.s@zluri.com>
2025-03-07 11:47:11 +0530

    update run-diff.sh to compare changed files against origin/main

--- scripts/run-diff.sh
+++ scripts/run-diff.sh
@@ -1,21 +1,7 @@
 #!/usr/bin/env sh
 
-# Get the current branch
-CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
-
-# Get the remote tracking branch i.e origin/main, <custom>/develop etc.
-# But the branch should be set as upstream, otherwise this will fail 
-# TODO: looking for alternatives
-REMOTE_BRANCH=$(git rev-parse --abbrev-ref --symbolic-full-name "@{u}" 2>/dev/null)
-
-# If there's no remote tracking branch, exit
-if [ -z "$REMOTE_BRANCH" ]; then
-  echo "No remote tracking branch found for $CURRENT_BRANCH, skipping tests."
-  exit 0
-fi
-
-# Get the list of changed files compared to the remote branch
-CHANGED_FILES=$(git diff --name-only "$REMOTE_BRANCH")
+# Get the list of changed files compared to the main branch
+CHANGED_FILES=$(git diff --name-only origin/main origin/${GITHUB_HEAD_REF})
 
 # Run Jest only on changed files if there are any
 if [ -n "$CHANGED_FILES" ]; then
